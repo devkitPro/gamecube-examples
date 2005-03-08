ada72b41222e798a3445bfb4ded67604a20cabeb
Dave Murphy <wntrmute@users.sourceforge.net>
2005-03-08 16:30:55 +0000

    *** empty log message ***

--- MemCardDemo/source/input.c
+++ MemCardDemo/source/input.c
@@ -1,80 +1,53 @@
 #include <gccore.h>
 #include <ogcsys.h>
 
+//---------------------------------------------------------------------------------
 typedef struct tagKeyInput{
-	vu16 Up,
-		Down,
-		Held,
-		Last,
-		DownRepeat;
+	vs16	up,
+			down,
+			state,
+			oldstate;
 	}__attribute__ ((packed)) KeyInput;
 
-static volatile KeyInput Keys = { 0,0,0,0,0 };
-PADStatus pad[4];
+static volatile KeyInput Keys[4] = { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+static PADStatus pad[4];
 
 
 //---------------------------------------------------------------------------------
-u16	KeysDown(void) {
+void ScanPads() {
 //---------------------------------------------------------------------------------
-	u16 tmp = Keys.Down;
-	Keys.DownRepeat = 0;
-	Keys.Down =0;
-	return tmp;
-}
 
-//---------------------------------------------------------------------------------
-u16	KeysUp(void) {
-//---------------------------------------------------------------------------------
-	u16 tmp = Keys.Up;
-	Keys.Up = 0;
-
-	return tmp;
-}
-
-//---------------------------------------------------------------------------------
-u16	KeysHeld(void) {
-//---------------------------------------------------------------------------------
-	return Keys.Held;
-}
-
-//---------------------------------------------------------------------------------
-void ScanKeys() {
-//---------------------------------------------------------------------------------
 	PAD_Read(pad);
-	Keys.Last = Keys.Held;
-	Keys.Held = pad[0].button;
+	int index;
 
-	if(pad[0].button&PAD_BUTTON_START) {
-		void (*reload)() = (void(*)())0x80001800;
-		reload();
-	}
+	for ( index = 0; index < 4; index++) {
 
-	u16 pressed = Keys.Held & ( ~Keys.Last);
+		Keys[index].oldstate	= Keys[index].state;
+		Keys[index].state		= pad[index].button;
+		Keys[index].up			= Keys[index].oldstate & ~Keys[index].state;
+		Keys[index].down		= ~Keys[index].oldstate & Keys[index].state;
+	}			
+}
 
-	Keys.DownRepeat	|=	pressed;
-	Keys.Down |= pressed;
+//---------------------------------------------------------------------------------
+u16 ButtonsUp(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
+	
+	return 	Keys[pad].up;
+}
+//---------------------------------------------------------------------------------
+u16 ButtonsDown(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
 
+	return 	Keys[pad].down;
+}
 
-	u16 released = ((~Keys.Held) & Keys.Last);
+//---------------------------------------------------------------------------------
+u16 ButtonsHeld(int pad) {
+//---------------------------------------------------------------------------------
+	if (pad<0 || pad >3) return 0;
 
-	Keys.Up		|=	released;
-
-	Keys.Down	&= ~released;
-	Keys.DownRepeat	&= ~released;
-
-	Keys.Up &= ~pressed;
-
-/*	if ( Keys.Last != Keys.Held) count = delay;
-
-
-	if ( delay != 0)
-	{
-		count--;
-		if (count == 0)
-		{
-			count = repeat;
-			Keys.DownRepeat |= Keys.Held;
-		}
-	}
-*/
+	return Keys[pad].state;
 }
--- MemCardDemo/source/main.c
+++ MemCardDemo/source/main.c
@@ -2,9 +2,13 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ogcsys.h>
+#include <time.h>
 
 #include "input.h"
+
+#define DemoFileName "MemCardDemo.dat"
 
 static void *xfb = NULL;
 
@@ -12,6 +16,8 @@ static u8 SysArea[CARD_WORKAREA] ATTRIBUTE_ALIGN(32);
 
 u32 first_frame = 1;
 GXRModeObj *rmode;
+void (*PSOreload)() = (void(*)())0x80001800;
+
 
 /*---------------------------------------------------------------------------------
 	This function is called if a card is physically removed
@@ -55,7 +61,7 @@ int main() {
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
 	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-	VIDEO_SetPreRetraceCallback(ScanKeys);
+	VIDEO_SetPreRetraceCallback(ScanPads);
 	console_init(xfb,20,64,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*2);
 
 	printf("Memory Card Demo\n\n");
@@ -64,7 +70,8 @@ int main() {
 		printf("Insert A card in slot B and press A\n");
 
 
-		while ( !(KeysDown()& PAD_BUTTON_A)) {
+		while ( !(ButtonsDown(0) & PAD_BUTTON_A)) {
+			if (ButtonsDown(0) & PAD_BUTTON_START) PSOreload();
 			VIDEO_WaitVSync();
 		}
 
@@ -76,21 +83,70 @@ int main() {
 		printf("slot B code %d\n",SlotB_error);
 
 		if (SlotB_error > 0) {
+
+			int SectorSize = 0;
+			CARD_GetSectorSize(CARD_SLOTB,&SectorSize);
+
+			printf("Sector size is %d bytes.\n\n",SectorSize);
+
+			char *CardBuffer = (char *)memalign(32,SectorSize);
+			
 			printf("Starting directory\n");
+
 			card_dir CardDir;
-		
+			card_file CardFile;
+			
 			int CardError = CARD_FindFirst(CARD_SLOTB, &CardDir);
-		
+
+			bool found = false;
+			
 			while ( CARD_ERROR_NOFILE != CardError ) {
 				printf("%s  %s  %s\n",CardDir.filename,CardDir.gamecode,CardDir.company);
 				CardError = CARD_FindNext(&CardDir);
+				if ( 0 == strcmp (DemoFileName, CardDir.filename)) found = true; 
 			};
 
 			printf("Finished directory\n\n");
 			
-			printf("writing test file ...\n");
+			if (found) {
+				printf("Test file contains :- \n");
+				//CardError = CARD_Open(CARD_SLOTB ,"MemCardDemo.bin",&CardFile);
+				//CARD_Read(&CardFile,CardBuffer,SectorSize,0);
+				//printf(CardBuffer);
+				
+				//CARD_Close(&CardFile);
 			
+			} else {
+			
+				printf("writing test file ...\n");
+
+				CARD_Unmount(CARD_SLOTB);
+				CARD_Init("DEMO","00");
+				int SlotB_error = CARD_Mount(CARD_SLOTB, SysArea, card_removed);
+
+				printf("mount code %d\n",SlotB_error);
+
+				CardError = CARD_Create(CARD_SLOTB ,DemoFileName,SectorSize,&CardFile);
+				
+				printf("error code %d\n",CardError);
+
+				if (0 == CardError) {
+					time_t gc_time;
+					gc_time = time(NULL);
+
+					sprintf(CardBuffer," This text was written by MemCardDemo at %s\n\0",ctime(&gc_time));
+
+					CardError = CARD_Write(&CardFile,CardBuffer,SectorSize,0);
+					printf("error code %d\n",CardError);
+					CardError = CARD_Close(&CardFile);
+					printf("error code %d\n",CardError);
+					}
+
+			}
+
 			CARD_Unmount(CARD_SLOTB);
+			free(CardBuffer);
+			
 		}
 	}
 
