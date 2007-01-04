d28258591ffe5ba75bfab0d126f85ba9ad0807e2
Dave Murphy <wntrmute@users.sourceforge.net>
2007-01-04 22:17:49 +0000

    add readsector example move memcard demo

---
+++ devices/dvd/readsector/source/dvd.c
@@ -0,0 +1,153 @@
+/****************************************************************************
+* libogc - DVD Demo
+*
+* Demonstrates reading a sector from DVD
+****************************************************************************/
+#include <stdio.h>
+#include <gccore.h>        /*** Wrapper to include common libogc headers ***/
+#include <ogcsys.h>        /*** Needed for console support ***/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+/*** 2D Video Globals ***/
+GXRModeObj *vmode;        /*** Graphics Mode Object ***/
+u32 *xfb = NULL;    /*** Framebuffer ***/
+ 
+int dvdstatus = 0;
+static u8 dvdbuffer[2048] ATTRIBUTE_ALIGN (32);    /*** One sector ***/
+dvdcmdblk cmdblk;
+ 
+/****************************************************************************
+* Initialise Video
+*
+* Before doing anything in libogc, it's recommended to configure a video
+* output.
+****************************************************************************/
+static void
+Initialise (void)
+{
+  VIDEO_Init ();        /*** ALWAYS CALL FIRST IN ANY LIBOGC PROJECT!
+                     Not only does it initialise the video 
+                     subsystem, but also sets up the ogc os
+                ***/
+ 
+  PAD_Init ();            /*** Initialise pads for input ***/
+ 
+    /*** Try to match the current video display mode
+         using the higher resolution interlaced.
+    
+         So NTSC/MPAL gives a display area of 640x480
+         PAL display area is 640x528
+    ***/
+  switch (VIDEO_GetCurrentTvMode ())
+    {
+    case VI_NTSC:
+      vmode = &TVNtsc480IntDf;
+      break;
+ 
+    case VI_PAL:
+      vmode = &TVPal528IntDf;
+      break;
+ 
+    case VI_MPAL:
+      vmode = &TVMpal480IntDf;
+      break;
+ 
+    default:
+      vmode = &TVNtsc480IntDf;
+      break;
+    }
+ 
+    /*** Let libogc configure the mode ***/
+  VIDEO_Configure (vmode);
+ 
+    /*** Now configure the framebuffer. 
+         Really a framebuffer is just a chunk of memory
+         to hold the display line by line.
+    ***/
+ 
+  xfb = (u32 *) MEM_K0_TO_K1 (SYS_AllocateFramebuffer (vmode));
+    /*** Define a console ***/
+  console_init (xfb, 20, 64, vmode->fbWidth, vmode->xfbHeight,
+        vmode->fbWidth * 2);
+ 
+    /*** Clear framebuffer to black ***/
+  VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
+  VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
+ 
+    /*** Set the framebuffer to be displayed at next VBlank ***/
+  VIDEO_SetNextFramebuffer (xfb);
+ 
+    /*** Get the PAD status updated by libogc ***/
+  VIDEO_SetPostRetraceCallback (PAD_ScanPads);
+  VIDEO_SetBlack (0);
+ 
+    /*** Update the video for next vblank ***/
+  VIDEO_Flush ();
+ 
+  VIDEO_WaitVSync ();        /*** Wait for VBL ***/
+  if (vmode->viTVMode & VI_NON_INTERLACE)
+    VIDEO_WaitVSync ();
+ 
+}
+ 
+/*---------------------------------------------------------------------------
+	Main
+
+	Mount a DVD and boot it
+---------------------------------------------------------------------------*/
+int main () {
+//--------------------------------------------------------------------------
+	int ret = 0;
+	int mounted = 0;
+	int *p;
+	int j, i;
+ 
+	Initialise ();    /*** Start video etc ***/
+	DVD_Init ();        /*** And the DVD subsystem ***/
+
+	printf ("libOGC DVD Example\n");
+	printf ("Mounting Disc\n");
+ 
+	/*** This will mount pretty much any disc, original or ISO ***/
+	mounted = DVD_Mount ();
+	printf ("OK\n");
+ 
+	/*** Read Sector 0 ***/
+	ret = DVD_ReadPrio (&cmdblk, dvdbuffer, 2048, 0, 2);
+
+	if (ret <= 0) {
+		printf ("Error during read sector 0\n");
+		while (1);
+	}
+ 
+	printf ("Read %d bytes\n", ret);
+	p = (int *) dvdbuffer;
+ 
+	/*** Identify disc type ***/
+	j = 0;
+
+	for (i = 0; i < 512; i++)
+		j += p[i];
+ 
+	if (j == 0) {
+		printf ("Disc is most probably ISO-9660\n");
+	} else {
+
+		if (memcmp (dvdbuffer + 32, "GAMECUBE \"EL TORITO\" BOOTLOADER", 31) == 0) {
+			printf ("Disc is gc-linux bootable\n");
+		} else {
+
+			if (memcmp (dvdbuffer, "COBRAMB1", 8) == 0) {
+				printf ("Disc is multigame image\n");
+			} else {
+				printf ("Disc is gcm\n%s\n", (char *) dvdbuffer + 32);
+			}
+		}
+	}
+ 
+	while (1);
+ 
+	return 0;
+}
---
+++ devices/memcard/MemCardDemo/source/main.c
@@ -0,0 +1,145 @@
+#include <gccore.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ogcsys.h>
+#include <time.h>
+
+#define DemoFileName "MemCardDemo.dat"
+
+static void *xfb = NULL;
+
+static u8 SysArea[CARD_WORKAREA] ATTRIBUTE_ALIGN(32);
+
+u32 first_frame = 1;
+GXRModeObj *rmode;
+void (*PSOreload)() = (void(*)())0x80001800;
+
+
+/*---------------------------------------------------------------------------------
+	This function is called if a card is physically removed
+---------------------------------------------------------------------------------*/
+void card_removed(s32 chn,s32 result) {
+//---------------------------------------------------------------------------------
+	printf("card was removed from slot %c\n",(chn==0)?'A':'B');
+	CARD_Unmount(chn);
+}
+
+//---------------------------------------------------------------------------------
+int main() {
+//---------------------------------------------------------------------------------
+	VIDEO_Init();
+	
+	switch(VIDEO_GetCurrentTvMode())
+	{
+		case VI_NTSC:
+			rmode = &TVNtsc480IntDf;
+			break;
+		case VI_PAL:
+			rmode = &TVPal528IntDf;
+			break;
+		case VI_MPAL:
+			rmode = &TVMpal480IntDf;
+			break;
+		default:
+			rmode = &TVNtsc480IntDf;
+			break;
+	}
+
+	PAD_Init();
+	DVD_Init();
+	printf("About to Mount\n");
+	DVD_Mount();
+	
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+		
+	VIDEO_Configure(rmode);
+		
+	VIDEO_SetNextFramebuffer(xfb);
+	VIDEO_SetBlack(FALSE);
+	VIDEO_Flush();
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+	VIDEO_SetPreRetraceCallback(PAD_ScanPads);
+	console_init(xfb,20,64,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*2);
+	VIDEO_SetNextFramebuffer(xfb);
+
+	printf("Memory Card Demo\n\n");
+
+	while (1) {
+		printf("Insert A card in slot B and press A\n");
+
+
+		while ( !(PAD_ButtonsDown(0) & PAD_BUTTON_A)) {
+			if (PAD_ButtonsDown(0) & PAD_BUTTON_START) PSOreload();
+			VIDEO_WaitVSync();
+		}
+
+		printf("Mounting card ...\n");
+
+		CARD_Init("DEMO","00");
+		int SlotB_error = CARD_Mount(CARD_SLOTB, SysArea, card_removed);
+	
+		printf("slot B code %d\n",SlotB_error);
+
+		int CardError;
+		
+		if (SlotB_error >= 0) {
+
+			unsigned int SectorSize = 0;
+			CARD_GetSectorSize(CARD_SLOTB,&SectorSize);
+
+			printf("Sector size is %d bytes.\n\n",SectorSize);
+
+			char *CardBuffer = (char *)memalign(32,SectorSize);
+			
+			printf("Starting directory\n");
+
+			card_dir CardDir;
+			card_file CardFile;
+			
+			CardError = CARD_FindFirst(CARD_SLOTB, &CardDir, true);
+
+			bool found = false;
+			
+			while ( CARD_ERROR_NOFILE != CardError ) {
+				printf("%s  %s  %s\n",CardDir.filename,CardDir.gamecode,CardDir.company);
+				CardError = CARD_FindNext(&CardDir);
+				if ( 0 == strcmp (DemoFileName, (char *)CardDir.filename)) found = true; 
+			};
+
+			printf("Finished directory\n\n");
+			
+			if (found) {
+				printf("Test file contains :- \n");
+				CardError = CARD_Open(CARD_SLOTB ,DemoFileName,&CardFile);
+				CARD_Read(&CardFile,CardBuffer,SectorSize,0);
+				printf("%s\n",CardBuffer);
+				
+				CARD_Close(&CardFile);
+			
+				CARD_Delete(CARD_SLOTB,DemoFileName);
+			} else {
+			
+				printf("writing test file ...\n");
+				CardError = CARD_Create(CARD_SLOTB ,DemoFileName,SectorSize,&CardFile);
+
+				if (0 == CardError) {
+					time_t gc_time;
+					gc_time = time(NULL);
+
+					sprintf(CardBuffer,"This text was written by MemCardDemo\nat %s\n",ctime(&gc_time));
+
+					CardError = CARD_Write(&CardFile,CardBuffer,SectorSize,0);
+					CardError = CARD_Close(&CardFile);
+				}
+			}
+
+			CARD_Unmount(CARD_SLOTB);
+			free(CardBuffer);
+			
+		}
+	}
+
+}
