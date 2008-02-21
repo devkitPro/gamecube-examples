04777a3723c0aad1dd92caac613df5396a3031ef
Dave Murphy <wntrmute@users.sourceforge.net>
2008-02-21 22:13:28 +0000

    update for r14

--- devices/dvd/readsector/source/dvd.c
+++ devices/dvd/readsector/source/dvd.c
@@ -1,102 +1,111 @@
-/****************************************************************************
-* libogc - DVD Demo
-*
-* Demonstrates reading a sector from DVD
-****************************************************************************/
+/*---------------------------------------------------------------------------------
+
+ libogc - DVD Demo
+
+ Demonstrates reading a sector from DVD
+
+---------------------------------------------------------------------------------*/
+
 #include <stdio.h>
-#include <gccore.h>        /*** Wrapper to include common libogc headers ***/
-#include <ogcsys.h>        /*** Needed for console support ***/
+#include <gccore.h>        // Wrapper to include common libogc headers
+#include <ogcsys.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
  
-/*** 2D Video Globals ***/
-GXRModeObj *vmode;        /*** Graphics Mode Object ***/
-u32 *xfb = NULL;    /*** Framebuffer ***/
- 
+// 2D Video Globals
+
+GXRModeObj *vmode;		// Graphics Mode Object
+u32 *xfb = NULL;		// Framebuffer  
 int dvdstatus = 0;
-static u8 dvdbuffer[2048] ATTRIBUTE_ALIGN (32);    /*** One sector ***/
+
+static u8 dvdbuffer[2048] ATTRIBUTE_ALIGN (32);    // One Sector
 dvdcmdblk cmdblk;
  
-/****************************************************************************
-* Initialise Video
-*
-* Before doing anything in libogc, it's recommended to configure a video
-* output.
-****************************************************************************/
-static void
-Initialise (void)
-{
-  VIDEO_Init ();        /*** ALWAYS CALL FIRST IN ANY LIBOGC PROJECT!
-                     Not only does it initialise the video 
-                     subsystem, but also sets up the ogc os
-                ***/
+/*---------------------------------------------------------------------------------
+ Initialise Video
+
+ Before doing anything in libogc, it's recommended to configure a video
+ output.
+---------------------------------------------------------------------------------*/
+static void Initialise () {
+//---------------------------------------------------------------------------------
+
+	VIDEO_Init ();	/*	ALWAYS CALL FIRST IN ANY LIBOGC PROJECT!
+						Not only does it initialise the video 
+						subsystem, but also sets up the ogc os
+					*/
  
-  PAD_Init ();            /*** Initialise pads for input ***/
+	PAD_Init ();	// Initialise pads for input
  
-    /*** Try to match the current video display mode
-         using the higher resolution interlaced.
-    
-         So NTSC/MPAL gives a display area of 640x480
-         PAL display area is 640x528
-    ***/
-  switch (VIDEO_GetCurrentTvMode ())
-    {
-    case VI_NTSC:
-      vmode = &TVNtsc480IntDf;
-      break;
+	
+	/*
+	
+	Try to match the current video display mode
+	using the higher resolution interlaced.
+	so NTSC/MPAL gives a display area of 640x480
+	PAL display area is 640x528
+
+	*/
+
+	switch (VIDEO_GetCurrentTvMode ()) {
+
+	case VI_NTSC:
+		vmode = &TVNtsc480IntDf;
+		break;
  
-    case VI_PAL:
-      vmode = &TVPal528IntDf;
-      break;
+	case VI_PAL:
+		vmode = &TVPal528IntDf;
+		break;
  
-    case VI_MPAL:
-      vmode = &TVMpal480IntDf;
-      break;
+	case VI_MPAL:
+		vmode = &TVMpal480IntDf;
+		break;
  
-    default:
-      vmode = &TVNtsc480IntDf;
-      break;
-    }
+	default:
+		vmode = &TVNtsc480IntDf;
+		break;
+	}
  
-    /*** Let libogc configure the mode ***/
-  VIDEO_Configure (vmode);
+	// Let libogc configure the mode
+	VIDEO_Configure (vmode);
  
-    /*** Now configure the framebuffer. 
-         Really a framebuffer is just a chunk of memory
-         to hold the display line by line.
-    ***/
+
+	/*
+	
+	Now configure the framebuffer. 
+	Really a framebuffer is just a chunk of memory
+	to hold the display line by line.
+	
+	*/
+
+	xfb = (u32 *) MEM_K0_TO_K1 (SYS_AllocateFramebuffer (vmode));
+
+
+	// Define a console
+	console_init (xfb, 20, 64, vmode->fbWidth, vmode->xfbHeight,
+	vmode->fbWidth * 2);
  
-  xfb = (u32 *) MEM_K0_TO_K1 (SYS_AllocateFramebuffer (vmode));
-    /*** Define a console ***/
-  console_init (xfb, 20, 64, vmode->fbWidth, vmode->xfbHeight,
-        vmode->fbWidth * 2);
+    // Clear framebuffer to black
+	VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
+	VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
+
+	/*** Set the framebuffer to be displayed at next VBlank ***/
+	VIDEO_SetNextFramebuffer (xfb);
  
-    /*** Clear framebuffer to black ***/
-  VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
-  VIDEO_ClearFrameBuffer (vmode, xfb, COLOR_BLACK);
+	VIDEO_SetBlack (0);
  
-    /*** Set the framebuffer to be displayed at next VBlank ***/
-  VIDEO_SetNextFramebuffer (xfb);
- 
-    /*** Get the PAD status updated by libogc ***/
-  VIDEO_SetPostRetraceCallback (PAD_ScanPads);
-  VIDEO_SetBlack (0);
- 
-    /*** Update the video for next vblank ***/
-  VIDEO_Flush ();
- 
-  VIDEO_WaitVSync ();        /*** Wait for VBL ***/
-  if (vmode->viTVMode & VI_NON_INTERLACE)
-    VIDEO_WaitVSync ();
+	// Update the video for next vblank 
+	VIDEO_Flush ();
+  
+	VIDEO_WaitVSync ();        // Wait for next Vertical Blank
+
+	if (vmode->viTVMode & VI_NON_INTERLACE)
+		VIDEO_WaitVSync ();
  
 }
  
-/*---------------------------------------------------------------------------
-	Main
-
-	Mount a DVD and boot it
----------------------------------------------------------------------------*/
+//---------------------------------------------------------------------------
 int main () {
 //--------------------------------------------------------------------------
 	int ret = 0;
@@ -104,17 +113,17 @@ int main () {
 	int *p;
 	int j, i;
  
-	Initialise ();    /*** Start video etc ***/
-	DVD_Init ();        /*** And the DVD subsystem ***/
+	Initialise ();		// Start video etc
+	DVD_Init ();		// And the DVD subsystem
 
 	printf ("libOGC DVD Example\n");
 	printf ("Mounting Disc\n");
  
-	/*** This will mount pretty much any disc, original or ISO ***/
+	// This will mount pretty much any disc, original or ISO
 	mounted = DVD_Mount ();
 	printf ("OK\n");
  
-	/*** Read Sector 0 ***/
+	// Read Sector 0
 	ret = DVD_ReadPrio (&cmdblk, dvdbuffer, 2048, 0, 2);
 
 	if (ret <= 0) {
@@ -125,7 +134,7 @@ int main () {
 	printf ("Read %d bytes\n", ret);
 	p = (int *) dvdbuffer;
  
-	/*** Identify disc type ***/
+	// Identify disc type
 	j = 0;
 
 	for (i = 0; i < 512; i++)
@@ -147,7 +156,19 @@ int main () {
 		}
 	}
  
-	while (1);
+	while(1) {
+
+		VIDEO_WaitVSync();
+		PAD_ScanPads();
+
+		int buttonsDown = PAD_ButtonsDown(0);
+		
+
+		if (buttonsDown & PAD_BUTTON_START) {
+			void (*reload)() = (void(*)())0x80001800;
+			reload();
+		}
+	}
  
 	return 0;
 }
