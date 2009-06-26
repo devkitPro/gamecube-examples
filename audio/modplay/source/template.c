69d72b58057bd96488c559e941ade638d622acb7
Dave Murphy <wntrmute@users.sourceforge.net>
2009-06-26 02:12:40 +0000

    add gamecube modplay example

---
+++ audio/modplay/source/template.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gccore.h>
+#include <asndlib.h>
+#include <gcmodplay.h>
+
+// include generated header
+#include "technique_mod.h"
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+static MODPlay play;
+
+//---------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+
+	// Initialise the video system
+	VIDEO_Init();
+	
+	// Initialise the attached controllers
+	PAD_Init();
+	
+	// Initialise the audio subsystem
+	ASND_Init(NULL);
+
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+	
+
+	printf("Hello World!");
+	
+	MODPlay_Init(&play);
+	MODPlay_SetMOD(&play,technique_mod);
+	MODPlay_Start(&play);
+
+
+
+	while(1) {
+
+		VIDEO_WaitVSync();
+		PAD_ScanPads();
+
+		int buttonsDown = PAD_ButtonsDown(0);
+		
+		if( buttonsDown & PAD_BUTTON_A ) {
+			printf("Button A pressed.\n");
+		}
+
+		if (buttonsDown & PAD_BUTTON_START) {
+			exit(0);
+		}
+	}
+
+	return 0;
+}
